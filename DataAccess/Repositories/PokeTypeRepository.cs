278df409dc1883c20faebf1b6938c7c05c18c98b
rkaminski <remigiusz20kaminski@gmail.com>
2025-11-17 23:16:22 +0100

    migrations

--- DataAccess/Repositories/PokeTypeRepository.cs
+++ DataAccess/Repositories/PokeTypeRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Abstractions;
 using Domain.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Repositories
 {
--- PocketTypeChart/Program.cs
+++ PocketTypeChart/Program.cs
@@ -1,6 +1,11 @@
+using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var cs = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<PokeDbContext>(opt => opt.useSqlServer(cs));
+builder.Services.AddDbContext<PokeDbContext>(opt => opt.UseSqlServer(cs));
 builder.Services.AddScoped<IPokeTypeRepository, PokeTypeRepository>();
 
 // Add services to the container.
